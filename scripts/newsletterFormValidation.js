25fa80f8cedd8f67ee80d42fe0e054a9b0d3e713
Camille Govehovitch <camillegovehovitch@MacBook-Air-de-Camille.local>
2020-04-17 17:21:31 +0200

    new validation regex

--- scripts/newsletterFormValidation.js
+++ scripts/newsletterFormValidation.js
@@ -1,12 +1,14 @@
 let newsletterInput = document.getElementById('newsletterInput');
 let submitBtn = document.getElementById('submitBtn');
-let email = document.getElementById('newsletterInput');
-console.log(newsletterInput.value);
+let emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
 function enableNewsletter(){
-    if(newsletterInput.value === "") {
+    if(!newsletterInput.value.match(emailRegex)) {
         submitBtn.disabled = true;
+        newsletterInput.classList.add("not-valid-email");
     }else{
         submitBtn.disabled = false;
+        newsletterInput.classList.remove("not-valid-email");
+        newsletterInput.classList.add("valid-email");
     }
 }
